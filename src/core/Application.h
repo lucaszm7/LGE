0908b784d6891d2a6e288995a11e06c824208937
Lucas Morais <lucas.morais446@hotmail.com>
2022-05-17 20:59:14 -0300

    feat: zoom, dragging and shader are default now (maybe change it later...)

--- src/core/Application.h
+++ src/core/Application.h
@@ -13,10 +13,13 @@
 // Queue
 std::vector<Vertex> PointsQueue;
 std::vector<Vertex> LinesQueue;
+std::vector<Vertex> RectQueue;
 
-void DrawPoint(float x, float y, float r, Color c = { 1.0f, 0.0f, 0.0f, 1.0f })
+
+void DrawPoint(float x, float y, float r = 50.0f, Color c = { 1.0f, 0.0f, 0.0f, 1.0f })
 {
     PointsQueue.emplace_back(x, y, c);
+    PointsRadius = r;
 }
 
 void DrawLine(float sx, float sy, float ex, float ey, Color c = { 1.0f, 0.0f, 0.0f, 1.0f })
@@ -25,9 +28,136 @@ void DrawLine(float sx, float sy, float ex, float ey, Color c = { 1.0f, 0.0f, 0.
     LinesQueue.emplace_back(ex, ey, c);
 }
 
+void DrawRect(const glm::vec2& vPos, const glm::vec2& vSize, Color c = { 1.0f, 0.0f, 0.0f, 1.0f })
+{
+    RectQueue.emplace_back(vPos.x, vPos.y, c);
+    RectQueue.emplace_back(vPos.x + vSize.x, vPos.y, c);
+    RectQueue.emplace_back(vPos.x + vSize.x, vPos.y + vSize.y, c);
+    RectQueue.emplace_back(vPos.x, vPos.y + vSize.y, c);
+}
+
 namespace LGE
 {
+    class TransformedView
+    {
+    public:
+        // ===== Algorithm =====
 
+        // World Offset Offset
+        float fOffsetX = 0.0f;
+        float fOffsetY = 0.0f;
+
+        // Difference for mouse picking
+        float fStartPanX = 0.0f;
+        float fStartPanY = 0.0f;
+
+        // Difference for zooming
+        float fScaleX = 1.0f;
+        float fScaleY = 1.0f;
+
+        bool bHeld = false;
+
+    public:
+
+        void WorldToScreen(float fWorldX, float fWorldY, int& nScreenX, int& nScreenY)
+        {
+            nScreenX = (int)((fWorldX - fOffsetX) * fScaleX);
+            nScreenY = (int)((fWorldY - fOffsetY) * fScaleY);
+        }
+
+        void ScreenToWorld(int nScreenX, int nScreenY, float& fWorldX, float& fWorldY)
+        {
+            fWorldX = ((float)(nScreenX) / fScaleX) + fOffsetX;
+            fWorldY = ((float)(nScreenY) / fScaleY) + fOffsetY;
+        }
+
+    public:
+        TransformedView()
+        {
+            fOffsetX = -(SCREEN_WIDTH / 2.0f);
+            fOffsetY = -(SCREEN_HEIGHT / 2.0f);
+        }
+
+        void HandleZoom()
+        {
+            double fMouseX, fMouseY;
+            LGE::GetCursorPos(fMouseX, fMouseY);
+
+            if ((LGE::GetMouseButton() == GLFW_PRESS) && !bHeld)
+            {
+                bHeld = true;
+                fStartPanX = (float)fMouseX;
+                fStartPanY = (float)fMouseY;
+            }
+
+            if (bHeld)
+            {
+                fOffsetX -= ((float)fMouseX - fStartPanX) / fScaleX;
+                fOffsetY -= ((float)fMouseY - fStartPanY) / fScaleY;
+
+                fStartPanX = (float)fMouseX;
+                fStartPanY = (float)fMouseY;
+            }
+
+            if ((LGE::GetMouseButton() == GLFW_RELEASE) && bHeld)
+            {
+                bHeld = false;
+                fOffsetX -= ((float)fMouseX - fStartPanX) / fScaleX;
+                fOffsetY -= ((float)fMouseY - fStartPanY) / fScaleY;
+
+                fStartPanX = (float)fMouseX;
+                fStartPanY = (float)fMouseY;
+            }
+
+            float fMouseWorldX_BeforeZoom, fMouseWorldY_BeforeZoom;
+            // Now I have the world position of my mouse
+            ScreenToWorld(fMouseX, fMouseY, fMouseWorldX_BeforeZoom, fMouseWorldY_BeforeZoom);
+
+            // Zooming with Q and E
+            if (LGE::GetKey(GLFW_KEY_E) == GLFW_PRESS)
+            {
+                fScaleX *= 1.003f;
+                fScaleY *= 1.003f;
+            }
+
+            if (LGE::GetKey(GLFW_KEY_Q) == GLFW_PRESS)
+            {
+                fScaleX *= 0.997f;
+                fScaleY *= 0.997f;
+            }
+
+
+            // ...now get the location of the cursor in world space again - It will have changed
+            // because the scale has changed, but we can offset our world now to fix the zoom
+            // location in screen space, because we know how much it changed laterally between
+            // the two spatial scales. Neat huh? ;-)
+            float fMouseWorldX_AfterZoom, fMouseWorldY_AfterZoom;
+            ScreenToWorld(fMouseX, fMouseY, fMouseWorldX_AfterZoom, fMouseWorldY_AfterZoom);
+
+            fOffsetX += (fMouseWorldX_BeforeZoom - fMouseWorldX_AfterZoom);
+            fOffsetY += (fMouseWorldY_BeforeZoom - fMouseWorldY_AfterZoom);
+
+        }
+
+        void Transform(std::vector<Vertex>& ve)
+        {
+            for (Vertex& p : ve)
+            {
+                int nScreenX, nScreenY;
+                WorldToScreen(p.Position.x, p.Position.y, nScreenX, nScreenY);
+                p.Position.x = nScreenX;
+                p.Position.y = nScreenY;
+            }
+        }
+    };
+
+}
+
+// Transformed View
+LGE::TransformedView tv;
+
+namespace LGE
+{
 
     class Application
     {
@@ -35,13 +165,20 @@ namespace LGE
         LGE::Menu* m_MainMenu;
         LGE::Scene_t* m_CurrentApp;
 
-    public:
+    protected:
         // Drawers
         std::unique_ptr<Drawer> DrawerPoints;
         std::unique_ptr<Drawer> DrawerLines;
+        std::unique_ptr<Drawer> DrawerRects;
+
+        // Transform
+        std::unique_ptr<Shader> m_Shader;
+        glm::mat4 m_Proj;
+
 
     public:
         Application()
+            : m_Proj(glm::ortho(0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, 0.0f, -1.0f, 1.0f))
         {
             Renderer::Init();
             m_CurrentApp = nullptr;
@@ -51,6 +188,10 @@ namespace LGE
 
             DrawerPoints = std::make_unique<Drawer>(SHAPE::POINT);
             DrawerLines = std::make_unique<Drawer>(SHAPE::LINE);
+            DrawerRects = std::make_unique<Drawer>(SHAPE::RECT);
+
+            m_Shader = std::make_unique<Shader>("res/shaders/Basic_2D.shader");
+            m_Shader->Bind();
         }
 
         template <typename T>
@@ -76,20 +217,31 @@ namespace LGE
                     m_MainMenu->c_SceneName = "Main Menu";
                     Renderer::ClearColor(0.0f, 0.0f, 0.25f, 1.0f);
                 }
+                tv.HandleZoom();
+                PointsRadius *= tv.fScaleX;
                 m_CurrentApp->OnUpdate(0.0f);
-
                 // Draw Primitives
                 if (LinesQueue.size() > 0)
                 {
+                    tv.Transform(LinesQueue);
                     DrawerLines->Draw(&LinesQueue[0], LinesQueue.size());
                     LinesQueue.clear();
                 }
                 if (PointsQueue.size() > 0)
                 {
+                    tv.Transform(PointsQueue);
                     DrawerPoints->Draw(&PointsQueue[0], PointsQueue.size());
                     PointsQueue.clear();
                 }
+                
+                if (RectQueue.size() > 0)
+                {
+                    tv.Transform(RectQueue);
+                    DrawerRects->Draw(&RectQueue[0], RectQueue.size());
+                    RectQueue.clear();
+                }
 
+                m_Shader->SetUniformMat4f("u_MVP", m_Proj);
                 m_CurrentApp->OnRender();
                 m_CurrentApp->OnImGuiRender();
                 ImGui::End();
@@ -104,4 +256,5 @@ namespace LGE
         }
 
     };
+
 }
