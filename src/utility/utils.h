dfbbf1baf2479ae0f5380e064fad47d3d755f97f
Lucas Morais <lucas.morais446@hotmail.com>
2022-05-18 22:14:20 -0300

    timer added

--- src/utility/utils.h
+++ src/utility/utils.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <iostream>
+#include <chrono>
 
 namespace LGE
 {
@@ -7,4 +8,29 @@ namespace LGE
 	{
 		return (start + static_cast<float> (std::rand()) / (static_cast <float> (RAND_MAX / (end - start))));
 	}
+
+	struct Timer
+	{
+		std::chrono::steady_clock::time_point start;
+		std::chrono::duration<double> duration;
+
+		Timer()
+		{
+			start = std::chrono::high_resolution_clock::now();
+		}
+
+		~Timer()
+		{
+			duration = std::chrono::high_resolution_clock::now() - start;
+			// std::cout << "\nTook: " << duration.count() * 1000.0 << "ms\n";
+		}
+
+		double now()
+		{
+			duration = std::chrono::high_resolution_clock::now() - start;
+			return duration.count() * 1000.0;
+		}
+
+	};
+
 }
